f528fec9c316b92214b5c8e410d06ea7e9ed7e59
HECyeah <hannahecarter@gmail.com>
2024-07-22 11:51:04 +0100

    nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,11 +6,11 @@ import './NavBar.css'
 export function NavBar() {
 
     return (
-        <div className="flex-initia">
-            <header >
-
+        <div >
+            <header className="flex flex-row items-center">
                 <img src={logo} alt="Page logo, who wants to be less of a pesent." />
-
+                <a className="p-2.5 text-slate-200 cursor-pointer" href="/">Home</a>
+                <a className="p-2.5 text-slate-200 cursor-pointer" href="/AboutUs">About Us</a>
             </header>
         </div>
     )
