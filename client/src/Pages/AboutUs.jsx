2ae3e2336ee24ef4f49938d4124007a11215869b
HECyeah <hannahecarter@gmail.com>
2024-07-22 14:35:12 +0100

    spell Check

--- client/src/Pages/AboutUs.jsx
+++ client/src/Pages/AboutUs.jsx
@@ -11,9 +11,9 @@ export function AboutUs() {
             <div className="flex flex-col py-5 text-slate-200">
                 <p className="py-5">One question and your life will never be the same again!</p>
 
-                <p className="py-5">Who Wants to Be Less of A Present is a never been seen online quiz. Work your way through a series of challenging questions in an attempt to win the grand prize - £15!</p>
+                <p className="py-5">Who Wants to Be Less of A Peasant is a never before seen online quiz. Work your way through a series of challenging questions in an attempt to win the grand prize - £15!</p>
 
-                <p className="py-5">Who Wants to Be Less of A Present should always be fun, playing responsibly.</p>
+                <p className="py-5">Who Wants to Be Less of A Peasant should always be fun, play responsibly.</p>
             </div>
         </div>
     )
