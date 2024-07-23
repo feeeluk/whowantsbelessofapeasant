1d1a642f9b7fc9523419c0f222bf0147722c9b09
HECyeah <hannahecarter@gmail.com>
2024-07-23 11:06:29 +0100

    basic clerk

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,6 +2,8 @@ import { Link, Route, Routes, BrowserRouter } from "react-router-dom"
 import logo from "./Picture2.png"
 import './NavBar.css'
 import { AboutUs } from "../Pages/AboutUs"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
 
 
 export function NavBar() {
@@ -14,7 +16,16 @@ export function NavBar() {
                     <li><Link to="/" className="p-2.5 text-slate-200 cursor-pointer">Home</Link></li>
                     <li><Link to="/aboutus" className="p-2.5 text-slate-200 cursor-pointer">About Us</Link></li>
                 </ol>
+                <div className="flex flex-row-reverse">
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </div>
             </header>
+
         </div>
     )
 }
