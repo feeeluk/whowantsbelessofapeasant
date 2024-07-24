bec4575c40c8f3d48e5c0c40843106dbde7719bd
bookishgallifreyan <05stanforthw@googlemail.com>
2024-07-24 15:30:05 +0100

    spelling changes, metadata, media query dabble

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -11,7 +11,7 @@ export function NavBar() {
     return (
         <div >
             <header className="flex flex-row items-center">
-                <img src={logo} alt="Page logo, who wants to be less of a pesent." />
+                <img src={logo} alt="Page logo -  Who wants to be less of a peasant?" />
                 <ol className="flex flex-row items-center">
                     <li><Link to="/" className="p-2.5 text-slate-200 cursor-pointer">Home</Link></li>
                     <li><Link to="/aboutus" className="p-2.5 text-slate-200 cursor-pointer">About Us</Link></li>
