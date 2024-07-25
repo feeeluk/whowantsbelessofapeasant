af6e1837acd4a3bf76ef9e65dd0b78deaa77247f
bookishgallifreyan <05stanforthw@googlemail.com>
2024-07-25 13:37:42 +0100

    new+improved

--- client/src/Pages/Quiz.jsx
+++ client/src/Pages/Quiz.jsx
@@ -1,68 +1,98 @@
-import { useParams } from "react-router-dom"
-import { useState, useEffect } from 'react'
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-export default function Quiz(){
+export default function Quiz() {
+  let [quiz, setQuiz] = useState(null);
+  let [question, setQuestion] = useState(0);
+  let [score, setScore] = useState(0);
 
-    let [quiz, setQuiz] = useState(null)
-    let [question, setQuestion] = useState(0)
-    let [score, setScore] = useState(0)
-    
-    const {id} = useParams()
+  const { id } = useParams();
 
-    function handleAnswerQuestion(answer){
-        if(answer === `${quiz[question].questions_final_answer}` && question === 14){
-            window.location.href = "http://localhost:5173/completed"
-        }
-        else if(answer === `${quiz[question].questions_final_answer}`) {
-            setScore(score + quiz[question].questions_value)
-            setQuestion(question + 1)
-        }
-        else{
-            window.location.href = "http://localhost:5173/gameover"    
-        }      
+  function handleAnswerQuestion(answer) {
+    if (
+      answer === `${quiz[question].questions_final_answer}` &&
+      question === 14
+    ) {
+      window.location.href = "http://localhost:5173/completed";
+    } else if (answer === `${quiz[question].questions_final_answer}`) {
+      setScore(score + quiz[question].questions_value);
+      setQuestion(question + 1);
+    } else {
+      window.location.href = "http://localhost:5173/gameover";
     }
+  }
 
-    function exitQuiz(){
-        window.location.href = "http://localhost:5173/exit"
+  function exitQuiz() {
+    window.location.href = "http://localhost:5173/exit";
+  }
+
+  useEffect(() => {
+    fetchQuiz();
+
+    async function fetchQuiz() {
+      const response = await fetch(`http://localhost:7070/quiz/${id}`);
+      const quizData = await response.json(response.rows);
+      setQuiz(quizData.reverse());
     }
+  }, [question]);
 
-    useEffect( () => {
-  
-        fetchQuiz()
-    
-        async function fetchQuiz(){
-          const response = await fetch(`http://localhost:7070/quiz/${id}`)
-          const quizData = await response.json(response.rows)
-          setQuiz(quizData.reverse())
-        }
+  return (
+    <div className="text-lg text-slate-300">
+      <h1>
+        Quiz {id} - {quiz ? <>{quiz[question].category_name}</> : ""}
+      </h1>
 
-      },[question])
+      {quiz ? (
+        <div className="">
+          <h2>Question number: {question + 1} / 15</h2>
+          <h2>Question score: {quiz[question].questions_value}pts</h2>
+          <h2>Question: {quiz[question].questions_question}</h2>
+          <div className="grid grid-cols-2 grid-rows-2 place-items-center max-w-96 place-content-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-48"
+              onClick={() => {
+                handleAnswerQuestion(quiz[question].questions_answer_1);
+              }}
+            >
+              A: {quiz[question].questions_answer_1}
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full  w-48"
+              onClick={() => {
+                handleAnswerQuestion(quiz[question].questions_answer_2);
+              }}
+            >
+              B: {quiz[question].questions_answer_2}
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full  w-48"
+              onClick={() => {
+                handleAnswerQuestion(quiz[question].questions_answer_3);
+              }}
+            >
+              C: {quiz[question].questions_answer_3}
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full  w-48"
+              onClick={() => {
+                handleAnswerQuestion(quiz[question].questions_answer_4);
+              }}
+            >
+              D: {quiz[question].questions_answer_4}
+            </button>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
 
-    return(
-        <div>
-            <h1>Quiz {id} - {quiz ? <>{quiz[question].category_name}</>:""}</h1>
+      <h1>Total quiz score: {score}</h1>
+      <button onClick={() => exitQuiz()}>EXIT</button>
 
-            {quiz ?
-                <div>
-                    <h2>Question number: {question + 1} / 15</h2>
-                    <h2>Question score: {quiz[question].questions_value}pts</h2>
-                    <h2>Question: {quiz[question].questions_question}</h2>
-                    <button onClick={ () => { handleAnswerQuestion(quiz[question].questions_answer_1) } }>Answer 1: {quiz[question].questions_answer_1}</button>
-                    <button onClick={ () => { handleAnswerQuestion(quiz[question].questions_answer_2) } }>Answer 2: {quiz[question].questions_answer_2}</button>
-                    <button onClick={ () => { handleAnswerQuestion(quiz[question].questions_answer_3) } }>Answer 3: {quiz[question].questions_answer_3}</button>
-                    <button onClick={ () => { handleAnswerQuestion(quiz[question].questions_answer_4) } }>Answer 4: {quiz[question].questions_answer_4}</button>
-                </div>
-                : ""
-            }
-
-            <h1>Total quiz score: {score}</h1>
-            <button onClick={()=> exitQuiz()}>EXIT</button>
-
-            {/* <h1>Lifelines:</h1>
+      {/* <h1>Lifelines:</h1>
             <button>Ask the audience</button>
             <button>50:50</button>
             <button>Phone a friend</button> */}
-
-        </div>
-    )
+    </div>
+  );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '0px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 1024px) { ... }
+
+      'lg': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
