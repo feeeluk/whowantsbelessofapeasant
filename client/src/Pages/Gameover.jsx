0ba72df0d7ea83d1afef7a3f39a356f200815a89
Philip Henning <philiphenning@outlook.com>
2024-07-25 14:24:12 +0100

    slight amend to gameover.jsx

--- client/src/Pages/Gameover.jsx
+++ client/src/Pages/Gameover.jsx
@@ -1,21 +1,20 @@
 export default function Gameover() {
-    function okContinue() {
-      window.location.href = "http://localhost:5173";
-    }
-  
-    return (
+
+  function okContinue(){
+      window.location.href = "http://localhost:5173"
+  } 
+
+  return (
       <div className="px-7">
-        <section className="text-5xl flex flex-row items-center  text-slate-200">
-          <h1>Unlucky..... that was the WRONG answer.</h1>
-        </section>
-        <div className="flex flex-col py-5 text-slate-200">
-          <p className="py-5">
-            Your score will be saved and added to the leaderboard.
-          </p>
-  
-          <button onClick={() => okContinue()}>Okay</button>
-        </div>
+          <section className="text-5xl flex flex-row items-center  text-slate-200">
+              <h1>Unlucky..... that was the WRONG answer.</h1>
+          </section>
+          <div className="flex flex-col py-5 text-slate-200">
+              <p className="py-5">Your score will be saved and added to the leaderboard.</p>
+
+              <button onClick={() => okContinue()}>Okay</button>
+          </div>
+
       </div>
-    );
-  }
-  
+  )
+}
